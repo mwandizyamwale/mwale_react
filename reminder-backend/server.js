af8cd70adb8478f742ac93bb9658ca53b56df4e6
Mwandizya Mwale <mmwandizya@gmail.com>
2026-05-08 09:57:49 -0800

    Change server listen address to '0.0.0.0'

--- reminder-backend/server.js
+++ reminder-backend/server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000
 
 async function start() {
   await initDB()
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`)
+ app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
   })
 }
 
